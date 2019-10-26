c4e238b8c5a21ded9709120fcdeb785973bf5cea
gabriel-roque <gabrielrms.dll@gmail.com>
2019-10-26 15:32:11 -0300

    [ADD] Interface Finalizada

--- web-app/src/components/CaixaEletronico.vue
+++ web-app/src/components/CaixaEletronico.vue
@@ -11,28 +11,36 @@
         <v-row justify="center">
             <v-col md="6" lg="10" xl="6">
                 <div class="pa-10 display-options">
-                    <v-row class="text-center">
+                    <v-row>
                         <v-col>
                             <v-row>
-                                <v-btn color="success" dark style="width: 150px" @click="depositar(this.valor)">Depositar</v-btn>
+                                <v-btn color="success" class="btn-display headline" @click="depositar">
+                                    Depositar
+                                </v-btn>
                             </v-row>
-                            <v-row class="mt-10">
-                                <v-btn color="error" dark style="width: 150px" @click="sacar(this.valor)">Sacar</v-btn>
+                            <v-row class="mt-15">
+                                <v-btn color="error" class="btn-display headline" @click="sacar">Sacar</v-btn>
                             </v-row>
                         </v-col>
-                        <v-spacer/>
                         <v-col cols="6">
-                            <div class="display-center pa-10">
-                                <label class="display-1">R$ {{valor}}</label>
-                            </div>
+                            <v-row>
+                                <div class="display-operator pa-3">
+                                    <label class="headline">Operação: <b>{{operacao}}</b></label>
+                                </div>
+                            </v-row>
+                            <v-row class="mt-3">
+                                <div class="display-center pa-6">
+                                    <label class="display-1 white--text">R$ {{valor.toLocaleString('pt-BR')}}</label>
+                                </div>
+                            </v-row>
+
                         </v-col>
-                        <v-spacer/>
                         <v-col>
                             <v-row>
-                                <v-btn color="warning" dark style="width: 150px">Bloquear </v-btn>
+                                <v-btn color="warning" class="btn-display headline" @click="bloquear">Bloquear</v-btn>
                             </v-row>
-                            <v-row class="mt-10">
-                                <v-btn color="primary" dark style="width: 150px">Desbloquear </v-btn>
+                            <v-row class="mt-15">
+                                <v-btn color="primary" class="btn-display headline" @click="desbloquear">Desbloquear</v-btn>
                             </v-row>
                         </v-col>
                     </v-row>
@@ -45,63 +53,75 @@
             <v-col md="8" lg="8" xl="5">
                 <v-row>
                     <v-col cols="2">
-                        <v-btn color="primary" class="button-numeric"><label class="display-1" @click="button('1')">1</label></v-btn>
+                        <v-btn color="primary" class="btn-numeric" @click="addDigit('1')">
+                            <label class="display-1">1</label></v-btn>
                     </v-col>
                     <v-col cols="2">
-                        <v-btn color="primary" class="button-numeric"><label class="display-1" @click="button('2')">2</label></v-btn>
+                        <v-btn color="primary" class="btn-numeric" @click="addDigit('2')">
+                            <label class="display-1">2</label></v-btn>
                     </v-col>
                     <v-col cols="2">
-                        <v-btn color="primary" class="button-numeric"><label class="display-1" @click="button('3')">3</label></v-btn>
+                        <v-btn color="primary" class="btn-numeric" @click="addDigit('3')">
+                            <label class="display-1">3</label></v-btn>
                     </v-col>
                     <v-col cols="6">
-                        <v-btn color="error" dark class="button-operador"><label class="display-1">Cancelar</label></v-btn>
+                        <v-btn color="error" class="btn-operator" @click="cancelar">
+                            <label class="display-1">Cancelar</label></v-btn>
                     </v-col>
                 </v-row>
                 <v-row>
                     <v-col cols="2">
-                        <v-btn color="primary" class="button-numeric"><label class="display-1" @click="button('4')">4</label></v-btn>
+                        <v-btn color="primary" class="btn-numeric" @click="addDigit('4')">
+                            <label class="display-1">4</label></v-btn>
                     </v-col>
                     <v-col cols="2">
-                        <v-btn color="primary" class="button-numeric"><label class="display-1" @click="button('5')">5</label></v-btn>
+                        <v-btn color="primary" class="btn-numeric" @click="addDigit('5')">
+                            <label class="display-1">5</label></v-btn>
                     </v-col>
                     <v-col cols="2">
-                        <v-btn color="primary" class="button-numeric"><label class="display-1" @click="button('6')">6</label></v-btn>
+                        <v-btn color="primary" class="btn-numeric" @click="addDigit('6')">
+                            <label class="display-1">6</label></v-btn>
                     </v-col>
                     <v-col cols="6">
-                        <v-btn color="warning" dark class="button-operador"><label class="display-1">Corrigir</label></v-btn>
+                        <v-btn color="warning" class="btn-operator" @click="corrgir">
+                            <label class="display-1">Corrigir</label></v-btn>
                     </v-col>
                 </v-row>
                 <v-row>
                     <v-col cols="2">
-                        <v-btn color="primary" class="button-numeric"><label class="display-1" @click="button('7')">7</label></v-btn>
+                        <v-btn color="primary" class="btn-numeric" @click="addDigit('7')">
+                            <label class="display-1">7</label></v-btn>
                     </v-col>
                     <v-col cols="2">
-                        <v-btn color="primary" class="button-numeric"><label class="display-1" @click="button('8')">8</label></v-btn>
+                        <v-btn color="primary" class="btn-numeric" @click="addDigit('8')">
+                            <label class="display-1">8</label></v-btn>
                     </v-col>
                     <v-col cols="2">
-                        <v-btn color="primary" class="button-numeric"><label class="display-1" @click="button('9')">9</label></v-btn>
+                        <v-btn color="primary" class="btn-numeric" @click="addDigit('9')">
+                            <label class="display-1">9</label></v-btn>
                     </v-col>
                     <v-col cols="6">
-                        <v-btn color="success" dark class="button-operador"><label class="display-1">Confirmar</label></v-btn>
+                        <v-btn color="success" dark class="btn-operator">
+                            <label class="display-1">Confirmar</label></v-btn>
                     </v-col>
                 </v-row>
                 <v-row>
                     <v-col cols="2">
-                        <v-btn color="blue-grey" class="button-numeric"><label class="display-1"></label></v-btn>
+                        <v-btn color="blue-grey" class="btn-numeric" disabled></v-btn>
                     </v-col>
                     <v-col cols="2">
-                        <v-btn color="primary" class="button-numeric"><label class="display-1" @click="button('0')">0</label></v-btn>
+                        <v-btn color="primary" class="btn-numeric" @click="addDigit('0')">
+                            <label class="display-1">0</label></v-btn>
                     </v-col>
                     <v-col cols="2">
-                        <v-btn color="blue-grey" class="button-numeric"><label class="display-1"></label></v-btn>
+                        <v-btn color="blue-grey" class="btn-numeric" disabled></v-btn>
                     </v-col>
                     <v-col cols="6">
-                        <v-btn color="blue-grey" class="button-operador"><label class="display-1"></label></v-btn>
+                        <v-btn color="blue-grey" class="btn-operator" disabled></v-btn>
                     </v-col>
                 </v-row>
             </v-col>
         </v-row>
-
 
 
     </v-container>
@@ -114,38 +134,82 @@
         data() {
             return {
                 conta: 1000,
-                statusConta: true,
-                valor: ''
+                statusBlock: true,
+                operacao: 'Nenhuma',
+                valor: 0
             }
         },
 
         computed: {
-            saldo(){
+            saldo() {
                 return this.conta
             }
         },
-
-
+        watch: {
+            valor(e) {
+                if (isNaN(e)) this.valor = 0;
+                let qtaDigitos = e.toString().match(/\d/g);
+                if(qtaDigitos.length === 12) this.valor = 0;
+            }
+        },
 
         methods: {
-            depositar(valor) {
-                return this.conta += valor
+            resetDisplay(){
+                this.valor = 0;
+            },
+
+            depositar() {
+                this.operacao = 'Depósito';
+                this.resetDisplay();
             },
 
             sacar(valor) {
-                return this.conta -= valor
+                this.operacao = 'Saque';
+                this.resetDisplay();
             },
 
-            button(valor) {
-                // eslint-disable-next-line no-use-before-define
-                // console.log(value)
-                this.valor += valor
+            bloquear(){
+                this.operacao = 'Conta Bloqueada';
+                this.statusBlock = false;
+                this.resetDisplay();
+            },
+
+            desbloquear(){
+                this.operacao = 'Conta Desbloqueada';
+                this.statusBlock = true;
+                this.resetDisplay();
+            },
+
+            cancelar(){
+                this.operacao = 'Operação Cancelada';
+                this.resetDisplay();
+            },
+
+            addDigit(digito) {
+
+                let valorDisplay = this.valor;
+
+                valorDisplay = valorDisplay.toString();
+
+                valorDisplay += digito;
+                valorDisplay = parseInt(valorDisplay);
+
+                this.valor = valorDisplay
+            },
+
+            corrgir() {
+                let valorDisplay = this.valor;
+
+                valorDisplay = valorDisplay.toString().match(/\d/g);
+
+                valorDisplay.pop();
+                valorDisplay = valorDisplay.toString().replace(/,/g, "");
+                valorDisplay = parseInt(valorDisplay);
+
+                this.valor = valorDisplay
+
             }
-
-
         }
-
-
     }
 </script>
 
@@ -166,27 +230,43 @@
         color: #4CAF50;
     }
 
-    .button-numeric {
+    .btn-numeric {
         height: 80px !important;
         width: 100px !important;
     }
 
-    .button-operador {
+    .btn-operator {
         width: 100%;
         height: 100% !important;
     }
 
+    .btn-display {
+        height: 50px !important;
+        width: 200px !important;
+    }
+
     .display-options {
         width: 100%;
-        height: 300px;
         background-color: #dcdcdc;
         border-radius: 15px;
         border: 5px solid rgba(0, 0, 0, 0.3) !important;
     }
 
+    .display-operator {
+        width: 100%;
+        margin: 0 25px;
+        background-color: rgba(225, 225, 225, 0.82);
+        border-radius: 15px;
+        border: 5px solid rgb(0, 235, 96) !important;
+    }
+
+    .mt-15 {
+        margin-top: 5em !important;
+    }
+
     .display-center {
         width: 100%;
-        height: 200px;
+        margin: 0 25px;
         background-color: rgba(79, 79, 79, 0.82);
         border-radius: 15px;
         border: 5px solid rgb(221, 184, 0) !important;
