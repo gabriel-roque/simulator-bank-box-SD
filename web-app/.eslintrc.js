f5395f3170bf5e56a4f61d8e14a83d9684e17973
gabriel-roque <gabrielrms.dll@gmail.com>
2019-10-26 15:31:52 -0300

    [ADD] Config do esLint

---
+++ web-app/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    rules: {
+        'no-console': 'off',
+    },
+    extends: [
+        'plugin:vue/recommended',
+    ],
+};
